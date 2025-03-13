608f1d4c2d95e4cb70c61ea4a6ff1334bea60aaa
Carda01 <1000021334@studium.unict.it>
2025-03-13 10:20:29 +0100

    removing headers

--- scripts/make.py
+++ scripts/make.py
@@ -8,19 +8,21 @@ from contextlib import ExitStack
 from datetime import datetime
 import numpy as np
 
-
 load_dotenv()
 
-# %%
 api_key = os.environ.get('API_KEY')
 headers = {"x-api-key": api_key}
 
-# %%
 def is_id_valid(paper):
     return paper is not None and not isinstance(paper, str) and paper['paperId'] is not None
 
+
+def get_corresponding_author_id(authors):
+    return authors[0]['authorId']
+
+
 def is_valid_paper(paper):
-    return paper['authors'] and paper['abstract'] is not None and paper['title'] is not None and paper['year'] is not None and paper['publicationTypes'] is not None and paper["publicationVenue"] is not None
+    return paper['authors'] and get_corresponding_author_id(paper['authors']) and paper['abstract'] is not None and paper['title'] is not None and paper['year'] is not None and paper['publicationTypes'] is not None and paper["publicationVenue"] is not None
 
 
 def is_valid_conference(paper):
@@ -31,40 +33,37 @@ def is_valid_journal(paper):
     return "JournalArticle" in paper["publicationTypes"] and paper["journal"] is not None and "name" in paper["journal"] and "pages" in paper["journal"] and "volume" in paper["journal"]
 
 
-def get_referencing_author_id(authors):
-    return authors[0]['authorId']
-
-# %% [markdown]
-# # CSV Needed
-# - paper (w abstract and relevant author) 
-# - paper-paper (n-n)
-# - author 
-# - paper-author (n-n)
-# - paper-reviewers (n-n)
-# - keywords
-# - paper-keywords (n-n)
-# - conference (1-n)
-# - journal (1-n)
-# - year (1-n)
-
-# %%
-csv_files = {
-    "paper": ["paperId","corpusId", "title", "referenceAuthorId", "abstract", "url", "publicationType", "publicationDate","publicationId","year"],
-    "paper_paper": ["citingPaperId", "citedPaperId"],
-    "author": ["authorId", "authorName"],
-    "author_paper": ["authorId", "paperId"],
-    "paper_relevant_author": ["paperId", "relevantAuthorId"],
-    "paper_reviewer": ["paperId", "reviewAuthorId"],
-    "paper_conference": ["paperId", "conferenceId"],
-    "paper_journal": ["paperId", "journalId"],
-    "keywords": ["keyword"],
-    "paper_keywords": ["paperId", "keyword"],
-    "conference": ["conferenceId", "conferenceName", "year", "cityId"],
-    "journal": ["journalId", "journalName", "journalPages", "journalVolume","year"],
-}
+csv_with_types = {
+        "paper": ["paperId:ID", "corpusId:string", "title:string", "abstract:string", "url:string", "publicationType:string", "publicationDate:string", "publicationId:string", "year:int"],
+        "paper_paper": ["citingPaperId:START_ID", "citedPaperId:END_ID"],
+        "author": ["authorId:ID", "authorName:string"],
+        "author_paper": ["authorId:START_ID", "paperId:END_ID"],
+        "paper_corresponding_author": ["paperId:START_ID", "correspondingAuthorId:END_ID"],
+        "paper_reviewer": ["paperId:START_ID", "reviewAuthorId:END_ID"],
+        "paper_conference": ["paperId:START_ID", "conferenceEditionId:END_ID"],
+        "paper_journal": ["paperId:START_ID", "journalId:END_ID"],
+        "keywords": ["keyword:ID"],
+        "paper_keywords": ["paperId:START_ID", "keyword:END_ID"],
+        "conference": ["conferenceId:ID", "conferenceName:string"],
+        "conference_edition": ["conferenceEditionId:ID", "year:int", "city:string"],
+        "conference_edition_conference": ["conferenceEditionId:START_ID", "conferenceId:END_ID"],
+        "journal": ["journalId:ID", "journalName:string", "journalPages:string", "journalVolume:int","year:int"],
+        }
 
 
-# %%
+csv_files = {}
+final_csv_headers = {}
+for key, value in csv_with_types.items():
+    csv_files[key] = []
+    final_csv_headers[key] = []
+    for column in value:
+        csv_files[key].append(column.split(":")[0])
+        if "_ID" in column:
+            final_csv_headers[key].append(':' + column.split(":")[1])
+        else:
+            final_csv_headers[key].append(column)
+
+
 #********************************************************************************************************************
 RECORDS = 100  # Number of records to save per category 
 QUERY = "semantic data modelling and property graphs"  # Query to filter the papers
@@ -80,7 +79,6 @@ response = requests.get(starting_papers_url, headers=headers).json()
 starting_papers = response["data"]
 
 
-# %%
 def process_new_papers(processed_papers, to_be_processed_papers, processing_papers, new_papers):
     new_papers.discard(None)
     for paper in new_papers:
@@ -91,7 +89,6 @@ def process_new_papers(processed_papers, to_be_processed_papers, processing_pape
 def choose_n_papers_to_process(to_be_processed_papers, n):
     return {to_be_processed_papers.pop() for _ in range(min(n, len(to_be_processed_papers)))}
 
-# %%
 BATCH_SIZE = 200
 MAX_RECURSION = 2
 csv_folder = Path('csv')
@@ -107,8 +104,9 @@ process_new_papers(processed_papers, to_be_processed_papers, set(), starting_pap
 set_authors = set()
 set_keywords = set()
 set_papers = set()
-set_joutnals = set()
+set_journals = set()
 set_conferences = set()
+set_conferences_edition = set()
 
 with ExitStack() as stack:  # Ensures all files are closed properly
     files = {name: stack.enter_context(open(csv_folder / (name + '.csv'), "w", newline='', encoding="utf-8")) for name in csv_files}
@@ -127,12 +125,12 @@ with ExitStack() as stack:  # Ensures all files are closed properly
         processing_papers_id = choose_n_papers_to_process(to_be_processed_papers, BATCH_SIZE)
 
         processing_papers_data = requests.post(
-            'https://api.semanticscholar.org/graph/v1/paper/batch',
-            params={'fields': FIELDS},
-            json={"ids": list(processing_papers_id)},
-            headers=headers
-        ).json()
-        
+                'https://api.semanticscholar.org/graph/v1/paper/batch',
+                params={'fields': FIELDS},
+                json={"ids": list(processing_papers_id)},
+                headers=headers
+                ).json()
+
         for paper in processing_papers_data:
             try:  
                 if not is_id_valid(paper):
@@ -147,21 +145,32 @@ with ExitStack() as stack:  # Ensures all files are closed properly
                     paper["journalName"]=None
                     paper["journalVolume"]=None
                     paper["journalPages"]=None
-                    conferenceId = paper["publicationId"] + str(paper["year"])
-                    if paper["publicationId"]+str(paper["year"]) not in set_conferences:
-                        set_conferences.add(paper["publicationId"]+str(paper["year"]))
-                        writers['conference'].writerow({
-                        "conferenceId": conferenceId,
-                        "conferenceName": paper["publicationVenue"]["name"],
-                        "year": paper["year"],
-                        "cityId": cities['id'].sample(n=1, replace=True).iloc[0]
-                        })
+                    if paper["publicationId"] not in set_conferences:
+                        set_conferences.add(paper["publicationId"])
+                        writers["conference"].writerow({
+                            "conferenceId": paper["publicationId"],
+                            "conferenceName": paper["publicationVenue"]["name"]
+                            })
+
+                    conferenceEditionId = paper["publicationId"] + str(paper["year"])
+                    if conferenceEditionId not in set_conferences_edition:
+                        set_conferences_edition.add(conferenceEditionId)
+                        writers['conference_edition'].writerow({
+                            "conferenceEditionId": conferenceEditionId,
+                            "year": paper["year"],
+                            "city": cities['city_ascii'].sample(n=1, replace=True).iloc[0]
+                            })
+
+                        writers['conference_edition_conference'].writerow({
+                            "conferenceEditionId": conferenceEditionId,
+                            "conferenceId": paper["publicationId"]
+                            })
 
                     writers["paper_conference"].writerow({
                         "paperId": paperId,
-                        "conferenceId": conferenceId
-                    })
-                    
+                        "conferenceEditionId": conferenceEditionId
+                        })
+
                 elif is_valid_journal(paper):
                     paper["publicationType"]="JournalArticle"
                     paper["venue"]=None
@@ -171,20 +180,20 @@ with ExitStack() as stack:  # Ensures all files are closed properly
                         paper["journalPages"]=re.sub(r'\s+', '', paper["journal"]["pages"])
                     else:
                         paper["journalPages"]=None
-                    if paper["publicationId"]+str(paper["year"]) not in set_joutnals:
-                        set_joutnals.add(paper["publicationId"]+str(paper["year"]))
+                    if paper["publicationId"]+str(paper["year"]) not in set_journals:
+                        set_journals.add(paper["publicationId"]+str(paper["year"]))
                         writers['journal'].writerow({
-                        "journalId": paper["publicationId"] + str(paper["year"]),
-                        "journalName": paper["journalName"],
-                        "journalVolume": paper["journalVolume"],
-                        "journalPages": paper["journalPages"], 
-                        "year": paper["year"],
-                        })
+                            "journalId": paper["publicationId"] + str(paper["year"]),
+                            "journalName": paper["journalName"],
+                            "journalVolume": paper["journalVolume"],
+                            "journalPages": paper["journalPages"], 
+                            "year": paper["year"],
+                            })
 
                     writers["paper_journal"].writerow({
                         "paperId": paperId,
                         "journalId": paper["publicationId"] + str(paper["year"])
-                    })
+                        })
 
                 else:
                     continue   
@@ -193,20 +202,19 @@ with ExitStack() as stack:  # Ensures all files are closed properly
                     "paperId": paperId,
                     "corpusId": paper.get("corpusId"),
                     "title":  paper.get("title").strip().replace("\n", " ").replace("|", " ").replace('"', "").replace("^", " "),
-                    "referenceAuthorId": get_referencing_author_id(paper_authors),
                     "abstract": paper.get("abstract").strip().replace("\n", " ").replace("|", " ").replace('"', "").replace("^", " "),
                     "url": paper.get("url"),
                     "year": paper.get("year"),
                     "publicationType": paper.get("publicationType"),
                     "publicationDate": paper.get("publicationDate"),
                     "publicationId": paper.get("publicationId")
-                })
+                    })
 
 
-                writers['paper_relevant_author'].writerow({
+                writers['paper_corresponding_author'].writerow({
                     "paperId": paperId,
-                    "relevantAuthorId": get_referencing_author_id(paper_authors)
-                })
+                    "correspondingAuthorId": get_corresponding_author_id(paper_authors)
+                    })
 
                 new_papers = set([paper['paperId'] for paper in paper['references']])
                 process_new_papers(processed_papers, to_be_processed_papers, processing_papers_id, new_papers)
@@ -215,7 +223,7 @@ with ExitStack() as stack:  # Ensures all files are closed properly
                     writers['paper_paper'].writerow({
                         "citingPaperId": paperId,
                         "citedPaperId": new_paper,
-                    })
+                        })
 
                 for author in paper_authors:
                     authorId = author.get("authorId")
@@ -224,37 +232,36 @@ with ExitStack() as stack:  # Ensures all files are closed properly
                         writers['author_paper'].writerow({
                             "authorId": authorId,
                             "paperId": paperId
-                        })
+                            })
 
                     if authorId and authorName and authorId not in set_authors:
                         writers['author'].writerow({
-                                "authorId": authorId,
-                                "authorName": authorName
-                        })
+                            "authorId": authorId,
+                            "authorName": authorName
+                            })
 
                         set_authors.add(authorId)
-            
+
                 paper_keywords = paper.get("s2FieldsOfStudy", [])
                 paper_keywords = set(map(lambda x: x['category'], paper_keywords))
 
                 for keyword in paper_keywords:
                     writers["paper_keywords"].writerow({
-                            "paperId": paperId,
-                            "keyword": keyword
+                        "paperId": paperId,
+                        "keyword": keyword
                         })
-                    
+
                     if keyword not in set_keywords:
                         writers["keywords"].writerow({
                             "keyword": keyword
-                        })
+                            })
 
                         set_keywords.add(keyword)
-                
+
             except json.JSONDecodeError as e:
                 print(f"Error decoding JSON: {e}")
 
 
-# %%
 papers_file = "csv/paper.csv"
 conferences_file = "csv/conference.csv"
 journals_file = "csv/journal.csv"
@@ -264,7 +271,6 @@ df_conferences=pd.read_csv(conferences_file, delimiter='|')
 df_journals=pd.read_csv(journals_file, delimiter='|')
 
 
-# %%
 conferences=list(set(df_conferences["conferenceId"].to_list()))
 
 split_index = len(conferences) // 2
@@ -282,8 +288,7 @@ df_conf.to_csv("csv/conference.csv", sep="|",index=False)
 df_workshops.to_csv("csv/workshop.csv", sep="|", index=False)
 df.to_csv("csv/paper.csv", sep="|", index=False)
 
-# %%
-#Cleaning references :)
+# Cleaning references
 citations_file="csv/paper_paper.csv"
 
 df_citations=pd.read_csv(citations_file, delimiter='|')
@@ -291,55 +296,11 @@ df_citations_new = df_citations[df_citations["citedPaperId"].isin(df['paperId'].
 df_citations_new.to_csv("csv/paper_paper.csv", sep='|', index=False)
 
 
-# %%
-url = "https://api.semanticscholar.org/graph/v1/author/batch"
-BATCH_SIZE = 500
-author_file="csv/author.csv"  
-query_params = {
-    "fields": "name,url,paperCount,hIndex"
-}
-
-df=pd.read_csv(author_file,  delimiter='|')
-ids=df["authorId"].values.tolist()
-df_copy = df.copy()
-
-
-with open(author_file, "w", newline='', encoding="utf-8") as outfile  :   
-    csv_writer_1 = csv.DictWriter(outfile, fieldnames=["authorId", "url", "authorName", "paperCount", "hIndex"], delimiter="|")
-    # Write the headers to the CSV files
-    csv_writer_1.writeheader()
-    for i in range(0, len(ids), BATCH_SIZE):
-        batch = ids[i:i + BATCH_SIZE]
-        data = {
-        "ids": batch
-        }
-        response = requests.post(url, params=query_params, json=data, headers=headers).json()
-        # Save the results to json file
-        count=0
-        for paper in response:
-            count+=1
-            try: 
-                if paper is not None: 
-                    paper_row = {
-                                "authorId": paper.get("authorId"),
-                                "url": paper.get("url"),
-                                "authorName": paper.get("name"),
-                                "paperCount": paper.get("paperCount"),
-                                "hIndex": paper.get("hIndex")
-                                }
-                    # Write the row to CSV 1
-                    csv_writer_1.writerow(paper_row)
-                else:
-                    paper_row = {
-                                "authorId":df_copy.loc[df_copy['authorId'] == batch[count-1], "authorId"].iloc(0),
-                                "url": None,
-                                "authorName": df_copy.loc[df_copy['authorId'] == batch[count-1], "authorName"].iloc(0),
-                                "paperCount": None,
-                                "hIndex": None
-                                }
-                    # Write the row to CSV 1
-                    csv_writer_1.writerow(paper_row)
-            except json.JSONDecodeError as e:
-                print(f"Error decoding JSON: {e}")
+for csv_name in csv_files.keys():
+    with open(csv_folder / (csv_name + '.csv'), 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(csv_folder / (csv_name + '.csv'), 'w') as fout:
+        fout.write("|".join(final_csv_headers[csv_name]) + '\n')
+        fout.writelines(data[1:])
 
 print("CSVs successfully created")
